35a5957acee4500c480553e3c9231bb1f7701a14
d2015-kamei <s2015@oita.kosen-ac.jp>
2022-12-31 16:41:40 +0900

    マイナス時の処理を変更

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ string nutrient_injection(int length);
 int main(int argc, char *argv[])
 {
     int length = 0;
+    bool is_nullpoga = false;
     cout << endl;
     if(argc > 1)
     {
@@ -16,26 +17,16 @@ int main(int argc, char *argv[])
         }
         catch(const std::invalid_argument& e)
         {
-            cout << "正月くらいまともな引数を入れてくれよ\n" << endl;
+            if(!((string)argv[1] == "NULL")) cout << "正月くらいまともな引数を入れてくれよ\n" << endl;
         }
         catch(const std::out_of_range& e)
         {
             cout << "正月くらい範囲内の値を入れてくれよ\n" << endl;
         }
+        if((string)argv[1] == "NULL" || (string)argv[1] == "0") is_nullpoga = true;
 
     }
-    
-    if(length >= 0)
-    {
-        cout << "     A       新年"     << endl;
-        cout << "   A|V|A   あけまして"   << endl; 
-        cout << "  |V|-|V|  おめでとう"   << endl;
-        cout << "  |-| |-|  ございます"   << endl;
-        cout << "  | |-| |"              << endl;
-        if(argc > 1)
-            cout << nutrient_injection(length);
-    }
-    else
+    if(is_nullpoga)
     {
         cout << "     A  " << endl;
         cout << "    |V|    " << endl;
@@ -47,10 +38,29 @@ int main(int argc, char *argv[])
         cout << "    |P|    " << endl;
         cout << "    |O|    Ga! >" << endl;
     }
+    else if(length >= 0)
+    {
+        cout << "     A       新年"     << endl;
+        cout << "   A|V|A   あけまして"   << endl; 
+        cout << "  |V|-|V|  おめでとう"   << endl;
+        cout << "  |-| |-|  ございます"   << endl;
+        cout << "  | |-| |"              << endl;
+        cout << nutrient_injection(length);
+    }
     cout << " (WWWWWWW)"            << endl;
     cout << " (¥¥¥¥¥¥¥)"            << endl;
     cout << " [=======] 2023年元旦" << endl;
     cout << " /#######\\"           << endl;
+    
+    if(length < 0)
+    {
+        cout << nutrient_injection(abs(length));
+        cout << "  |A| |A|  ございます"     << endl;
+        cout << "   V|A|V   おめでとう"     << endl;
+        cout << "     V     あけまして"   << endl;
+        cout << "             新年"     << endl;
+
+    }
     cout << endl;
     return 0;
 }
